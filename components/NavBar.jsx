401dc67384f54b5f0f4328a230509a7360ea2fbe
madara <wcamacho221@gmail.com>
2021-03-03 23:28:05 -0500

    NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,14 +8,27 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 const NavBar = () => {
     return (
         <>
-            <div className="bg-white py-2 shadow-lg">
-                <div className="flex justify-around w-11/12 m-auto items-center">
-                    <Link href="/"><a><img src="/logotipomodificado.png" alt="logo" width="40" /></a></Link>
-                    <div className={styles.search}>
-                        <div className={styles.searchIcon}>
-                            <FontAwesomeIcon icon={faSearch} className="text-gray-400 hover:text-gray-500" />
+            <div className="bg-white py-1 shadow-lg">
+                <div className="flex  w-11/12 m-auto items-center justify-between">
+                    <div className="flex items-center space-x-8">
+                        <Link href="/"><a><img src="/logotipomodificado.png" alt="logo" width="40" /></a></Link>
+                        <div className={styles.search}>
+                            <div className={styles.searchIcon}>
+                                <FontAwesomeIcon icon={faSearch} className="text-gray-400 hover:text-gray-500" />
+                            </div>
+                            <input type="text" placeholder="Busca el accesario" className={styles.inputInput} />
                         </div>
-                        <input type="text" placeholder="Busca el accesario" className={styles.inputInput}/>
+                    </div>
+                    <div className="">
+                        <nav>
+                            <Link href="/">
+                                <a>
+                                    <div>
+                                        
+                                    </div>
+                                </a>
+                            </Link>
+                        </nav>
                     </div>
                 </div>
             </div>
